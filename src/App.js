a0a09c310ddeab16d6e926e37a81aad03f5bfafd
FacundoVillarroel <facu.villarroel96@gmail.com>
2022-11-29 00:06:37 -0300

    [Setup] - Fonts Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [loaded] = useFonts({
+    "Anek-Light" : require("../assets/fonts/AnekDevanagari-Light.ttf"),
+    "Anek-Regular" : require("../assets/fonts/AnekDevanagari-Regular.ttf"),
+    "Anek-SemiBold" : require("../assets/fonts/AnekDevanagari-SemiBold.ttf"),
+    "Anek-Bold" : require("../assets/fonts/AnekDevanagari-Bold.ttf"),
+  })
+
+  if (!loaded) {
+    return (
+      <View styles={styles.container}>
+        <ActivityIndicator color="#DAC4F7" size="large" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +32,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
